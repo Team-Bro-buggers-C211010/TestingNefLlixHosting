5b833efb10d5edb6a80289d16e75aa4cd7b0f316
Team-Bro-buggers-C211010 <shovancse.iiuc.cp@gmail.com>
2023-12-04 23:51:15 +0600

    movie row added & change in css

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
-import endpoints from '../services/movieServices';
+import endpoints, { createImageUrl } from '../services/movieServices';
 
 const Hero= ()=> {
     const [movie,setMovie]=useState({});
@@ -28,7 +28,7 @@ const Hero= ()=> {
         <div className="w-full h-full">
             {/* Shadow effect in home banner */}
             <div className="absolute w-full h-[550px] lg:h-[850px] bg-gradient-to-r from-black" />
-                <img className="w-full h-full object-cover object-top" src={`https://image.tmdb.org/t/p/original/${backdrop_path}`} alt={title} />
+                <img className="w-full h-full object-cover object-top" src={createImageUrl(backdrop_path,"original")} alt={title} />
                 <div className="absolute w-full top-[20%] lg:top-[40%] p-4 md:p-8">
                     <h1 className="text-2xl md:text-4xl font-nsans-bold">{title}</h1>
                     <div className="mt-8 mb-4">
@@ -39,7 +39,6 @@ const Hero= ()=> {
                     <p className="w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[40%] text-gray-200">{truncate(overview,300)}</p>
                 </div>
         </div>
-
     </div>
   );
 }
