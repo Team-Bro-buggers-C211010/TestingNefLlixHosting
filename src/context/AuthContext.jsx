a7f195398e6cc49e0dbe254ce016242a58033a11
Shahriar789 <shahriar.ifti.01@gmail.com>
2023-12-08 18:19:27 +0600

    Auth update

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,8 +1,21 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
 
+import { 
+    createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged,
+} from "firebase/auth";
+import { auth, db } from "../services/firebase";
 const AuthContext = createContext();
 
 export function AuthContextProvider({children}) {
+   
+    const [user , setUser] = useState({});
+
+    function signup(email, password){
+        createUserWithEmailAndPassword
+    }
+   
+
+
   return <AuthContext.Provider value={{}}>(children)</AuthContext.Provider>;
 }
 
